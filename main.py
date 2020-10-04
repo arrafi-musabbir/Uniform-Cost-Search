3991dce00e47165f6db14c963ec072a166310c93
arrafi-musabbir <arrafi-musabbir@github.com>
2020-10-04 17:29:34 +0600

    Runner script. Input the text file path here.

--- main.py
+++ main.py
@@ -6,12 +6,7 @@ Created on Sat Oct 02 17:13:02 2020
 
 import UCS_algo as U
 
-<<<<<<< HEAD
 if __name__ == "__main__":
     u = U.UCS(input("Text file path: "), 's', 'g')
 else:
     print("Invalid Module")
-=======
-f = FileRead.FileRead('D:/github/Uniform-Cost-Search/Weighted graph(cities).txt')
->>>>>>> a325d18f2adb024dfef8257973ca5f589b88c41b
-
